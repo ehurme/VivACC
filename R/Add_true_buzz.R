97f7627bad0f048859344423d68bf470784602a3
Edward Hurme <edwardhurme@gmail.com>
2021-07-06 16:30:42 +0200

    true buzz

--- R/Add_true_buzz.R
+++ R/Add_true_buzz.R
@@ -6,7 +6,8 @@ options(digits.secs = 3)
 # get buzz clip times
 buzz_clips <- list.files("C:/Users/Edward/Dropbox/VivesiAudio/BuzzCheck/Mviv17/Mviv17_33/Buzz")
 split_time <- strsplit(buzz_clips, split = "_")
-buzz_times <- data.frame(start_clip = rep(ymd_hms("2000-01-01 12:00:00.001"), length(buzz_clips)), 
+buzz_times <- data.frame(file = buzz_clips,
+  start_clip = ymd_hms("2000-01-01 12:00:00.001"), 
                                           segment = NA)
 
 for(i in 1:length(split_time)){
@@ -20,7 +21,7 @@ for(i in 1:length(split_time)){
 #################################################################################################
 # manually add true buzz times
 # reload buzz times with true buzzes added
-buzz_times <- read.csv("../../../Dropbox/VivesiACC/data/truebuzz/Mviv17_33_truebuzz.csv")
+buzz_times <- read.csv("../../../Dropbox/VivesiACC/data/truebuzz/Mviv17_33_truebuzz_offset.csv")
 buzz_times$start_clip <- ymd_hms(buzz_times$start_clip)
 
 buzz_times$true_buzz <- buzz_times$start_clip + buzz_times$offset + buzz_times$segment*0.5
@@ -33,11 +34,19 @@ Metrics$datetime <- ymd_hms(Metrics$datetime)+7*3600
 # add true_buzzes
 Metrics$true_buzz <- 0
 
-i = 1
-for(i in 1:length(na.omit(buzz_times$true_buzz))){
+i = 6
+# for(i in 1:length(na.omit(buzz_times$true_buzz))){
+  buzz <- 
+    which.min(abs(Metrics$datetime - (buzz_times$true_buzz[i])))
   true_start <- 
-    which.min(abs(Metrics$datetime - (buzz_times$start_clip[i] - 0.25)))
+    which.min(abs(Metrics$datetime - (buzz_times$true_buzz[i] - 0.25)))
   true_end <- 
-    which.min(abs(Metrics$datetime - (buzz_times$start_clip[i] + 0.25)))
+    which.min(abs(Metrics$datetime - (buzz_times$true_buzz[i] + 0.25)))
   Metrics$true_buzz[true_start:true_end] <- 1
-}
+  
+  with(Metrics[(buzz-100):(buzz+100),], plot(datetime, VeDBA, type = "l",
+                                             ylim = c(-5, max(VeDBA))))
+  with(Metrics[true_start:true_end,], points(datetime, col = "red", 
+                                             y = rep(0, length(true_start:true_end))))
+  with(Metrics[(buzz-100):(buzz+100),], points(datetime, y = rep(-3, 201), col = buzz))
+# }
